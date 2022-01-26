c5284e3bb7dc8621838cdf13d34ae78339b0748f
Gameknight <83509046+Gameknight-Bit@users.noreply.github.com>
2022-01-25 21:16:57 -0500

    Updated 256SHAReplica.py
    Fixed Bug #1 and #2

--- 256SHAReplicaHash.py
+++ 256SHAReplicaHash.py
@@ -1,5 +1,7 @@
+import math
+
 ### Constants ###
-key = 'hello world'
+key = 'Hello My Name Is JACE'
 #################
 
 ### Helper Functions ###
@@ -8,7 +10,10 @@ def hexToBinary(hexNum):
 
 def binaryToHex(b):
     dec = int(b, 2)
-    return str(hex(dec))[2:]
+    hexThing = str(hex(dec))[2:]
+    while len(hexThing)%8 != 0: #Padding
+        hexThing = "0"+hexThing
+    return hexThing
 
 ##### BINARY MUTATOR FUNCTIONS #####
 def padBinary(b):
@@ -133,10 +138,8 @@ def hash(k): #Main Hashing Algorithm
     binaryKey = ''.join(format(ord(i), '08b') for i in k) #could also use bytearray
     binaryKey += '10000000' #append 1 to end
 
-    while(len(binaryKey)%(512-64) != 0):
+    while((len(binaryKey)+64)%512 != 0):
         binaryKey += '00000000'
-
-    #SPLIT BINARY INTO 512-64 bit sections!!!!!#
 
     binLen = '{0:064b}'.format(len(''.join(format(ord(i), '08b') for i in k))) #replace k with section
     if len(binLen) > 64:
@@ -144,62 +147,72 @@ def hash(k): #Main Hashing Algorithm
     else:
         binaryKey += binLen
 
+    #SPLIT BINARY INTO 512 bit sections!!!!!#
+    chunks = []
+    for o in range(math.floor(len(binaryKey)/512)):
+        chunks.append(binaryKey[:512])
+        if len(binaryKey) <= 512:
+            binaryKey = ""
+        else:
+            binaryKey = binaryKey[512:]
+
     #Done with main processing    
 
-    mSchedule = []
-    scheduleLen = 0
-    while scheduleLen < 16: #Word Processing (32-bit sections)
-        mSchedule.append(binaryKey[scheduleLen*32:(scheduleLen+1)*32])
-        scheduleLen += 1
+    for chunk in chunks:
+        mSchedule = []
+        scheduleLen = 0
+        while scheduleLen < 16: #Word Processing (32-bit sections)
+            mSchedule.append(chunk[scheduleLen*32:(scheduleLen+1)*32])
+            scheduleLen += 1
 
-    for _ in range(48):
-        mSchedule.append('00000000000000000000000000000000')
+        for _ in range(48):
+            mSchedule.append('00000000000000000000000000000000')
 
-    #Modifying Zero Indexes#
-    i = 16
-    while i < len(mSchedule):
-        t1 = xor(xor(rightrotate(mSchedule[i-15], 7), rightrotate(mSchedule[i-15], 18)), rightshift(mSchedule[i-15], 3))
-        t2 = xor(xor(rightrotate(mSchedule[i-2], 17), rightrotate(mSchedule[i-2], 19)), rightshift(mSchedule[i-2], 10))
+        #Modifying Zero Indexes#
+        i = 16
+        while i < len(mSchedule):
+            t1 = xor(xor(rightrotate(mSchedule[i-15], 7), rightrotate(mSchedule[i-15], 18)), rightshift(mSchedule[i-15], 3))
+            t2 = xor(xor(rightrotate(mSchedule[i-2], 17), rightrotate(mSchedule[i-2], 19)), rightshift(mSchedule[i-2], 10))
 
-        mSchedule[i] = padBinary32(add(add(add(mSchedule[i-16], t1), mSchedule[i-7]), t2))
-        i += 1 #counter
-    #################################
+            mSchedule[i] = padBinary32(add(add(add(mSchedule[i-16], t1), mSchedule[i-7]), t2))
+            i += 1 #counter
+        #################################
 
-    #Compression#
-    a = hexToBinary(hash0)
-    b = hexToBinary(hash1)
-    c = hexToBinary(hash2)
-    d = hexToBinary(hash3)
-    e = hexToBinary(hash4)
-    f = hexToBinary(hash5)
-    g = hexToBinary(hash6)
-    h = hexToBinary(hash7)
+        #Compression#
+        a = hexToBinary(hash0)
+        b = hexToBinary(hash1)
+        c = hexToBinary(hash2)
+        d = hexToBinary(hash3)
+        e = hexToBinary(hash4)
+        f = hexToBinary(hash5)
+        g = hexToBinary(hash6)
+        h = hexToBinary(hash7)
 
-    for x in range(64):
-        s1 = xor(xor(rightrotate(e, 6), rightrotate(e, 11)), rightrotate(e, 25))
-        ch = xor(andb(e, f), andb(notb(e), g))
-        temp1 = add(add(add(add(h, s1), ch), padBinary32("{0:b}".format(int(kConstants[x])))), mSchedule[x])
-        s0 = xor(xor(rightrotate(a, 2), rightrotate(a, 13)), rightrotate(a, 22))
-        maj = xor(xor(andb(a, b), andb(a, c)), andb(b, c))
-        temp2 = add(s0, maj)
+        for x in range(64):
+            s1 = xor(xor(rightrotate(e, 6), rightrotate(e, 11)), rightrotate(e, 25))
+            ch = xor(andb(e, f), andb(notb(e), g))
+            temp1 = add(add(add(add(h, s1), ch), padBinary32("{0:b}".format(int(kConstants[x])))), mSchedule[x])
+            s0 = xor(xor(rightrotate(a, 2), rightrotate(a, 13)), rightrotate(a, 22))
+            maj = xor(xor(andb(a, b), andb(a, c)), andb(b, c))
+            temp2 = add(s0, maj)
 
-        h=g
-        g=f
-        f=e
-        e=padBinary32(add(d, temp1))
-        d=c
-        c=b
-        b=a
-        a=padBinary32(add(temp1, temp2))
-    
-    hash0 = binaryToHex(padBinary32(add(hexToBinary(hash0), a)))
-    hash1 = binaryToHex(padBinary32(add(hexToBinary(hash1), b)))
-    hash2 = binaryToHex(padBinary32(add(hexToBinary(hash2), c)))
-    hash3 = binaryToHex(padBinary32(add(hexToBinary(hash3), d)))
-    hash4 = binaryToHex(padBinary32(add(hexToBinary(hash4), e)))
-    hash5 = binaryToHex(padBinary32(add(hexToBinary(hash5), f)))
-    hash6 = binaryToHex(padBinary32(add(hexToBinary(hash6), g)))
-    hash7 = binaryToHex(padBinary32(add(hexToBinary(hash7), h)))
+            h=g
+            g=f
+            f=e
+            e=padBinary32(add(d, temp1))
+            d=c
+            c=b
+            b=a
+            a=padBinary32(add(temp1, temp2))
+        
+        hash0 = binaryToHex(padBinary32(add(hexToBinary(hash0), a)))
+        hash1 = binaryToHex(padBinary32(add(hexToBinary(hash1), b)))
+        hash2 = binaryToHex(padBinary32(add(hexToBinary(hash2), c)))
+        hash3 = binaryToHex(padBinary32(add(hexToBinary(hash3), d)))
+        hash4 = binaryToHex(padBinary32(add(hexToBinary(hash4), e)))
+        hash5 = binaryToHex(padBinary32(add(hexToBinary(hash5), f)))
+        hash6 = binaryToHex(padBinary32(add(hexToBinary(hash6), g)))
+        hash7 = binaryToHex(padBinary32(add(hexToBinary(hash7), h)))
 
     return (hash0+hash1+hash2+hash3+hash4+hash5+hash6+hash7)
 
@@ -207,3 +220,4 @@ def hash(k): #Main Hashing Algorithm
 import hashlib
 
 print(hash(key))
+print(hashlib.sha256(key.encode()).hexdigest())
